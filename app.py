10b7718a6481727e766dc8151d302b8cc27cc269
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-05-03 12:51:04 -0400

    got a line working. get_exercises.html is not receving forms well

--- app.py
+++ app.py
@@ -11,10 +11,16 @@ app = Flask(__name__)
 ##THE THING IN QUOTES IS THE KEY
 ## ACTION = IN THE HTML FILE AUTOMATICALLY GOES TO THE NEXT ROUTE!!! FUCCCKKKK
 
+
+time_prompt = 0
+
 @app.route('/', methods=['GET', 'POST'])
-def get_time():
+def first_page():
+    global time_prompt
     if request.method == 'POST':
-        time_prompt = request.args.get('time')
+        time_prompt = request.form['time_prompt_form']
+        # time_prompt = request.args.get('time')
+
         return render_template('say_time.html', time_prompt=time_prompt)
 
     else:
@@ -22,15 +28,22 @@ def get_time():
 
 
 @app.route('/get_exercises', methods=['GET','POST'])
-def get_exercises():
+def second_page():
     if request.method == 'POST':
-        time_prompt = request.form['time_prompt']
-        exercise1 = request.args.get('exercise_1')
-        exercise2 = request.args.get('exercise_2')
-        return render_template('tester.html', exercise1=exercise1, exercise2=exercise2, time_prompt=time_prompt)
+        exercise1 = request.form['exercise1_form']
+        exercise2 = request.form['exercise2_form']
+
+        return render_template('tester.html', exercise1=exercise1, exercise2=exercise2)
 
     else:
-        return render_template('say_time.html')
+        return render_template('tester2.html')
+#         time_prompt = request.form['time_prompt']
+#         exercise1 = request.args.get('exercise_1')
+#         exercise2 = request.args.get('exercise_2')
+#         return render_template('tester.html', exercise1=exercise1, exercise2=exercise2, time_prompt=time_prompt)
+#
+#     else:
+#         return render_template('say_time.html')
 
     # '''<h4>The first exercise is {} and its interals are {}<h4>
     # <h4>The second exercise is {} and its internals are {}<h4>'''.format(exercise1, exercises[exercise1], exercise2, exercises[exercise2])
