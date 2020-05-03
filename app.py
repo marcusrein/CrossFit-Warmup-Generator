01018d9c4ef5a9e66d675e1b0ffdd71ea18696d5
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-05-03 08:02:38 -0400

    tried another tutorial. this ones helping. got the action=/ cue in the HTML.
    https://www.youtube.com/watch?v=IIi6e5oDZ68

--- app.py
+++ app.py
@@ -15,19 +15,25 @@ app = Flask(__name__)
 def get_time():
     if request.method == 'POST':
         time_prompt = request.args.get('time')
-        
-        return render_template('get_time.html', time_prompt=time_prompt)
+        return render_template('say_time.html', time_prompt=time_prompt)
 
     else:
         return render_template('index.html')
 
 
-@app.route('/get_exercises')
+@app.route('/get_exercises', methods=['GET','POST'])
 def get_exercises():
-    exercise1 = request.args.get('exercise_1')
-    exercise2 = request.args.get('exercise_2')
-    return '''<h4>The first exercise is {} and its interals are {}<h4>
-    <h4>The second exercise is {} and its internals are {}<h4>'''.format(exercise1, exercises[exercise1], exercise2, exercises[exercise2])
+    if request.method == 'POST':
+        time_prompt = request.form['time_prompt']
+        exercise1 = request.args.get('exercise_1')
+        exercise2 = request.args.get('exercise_2')
+        return render_template('tester.html', exercise1=exercise1, exercise2=exercise2, time_prompt=time_prompt)
+
+    else:
+        return render_template('say_time.html')
+
+    # '''<h4>The first exercise is {} and its interals are {}<h4>
+    # <h4>The second exercise is {} and its internals are {}<h4>'''.format(exercise1, exercises[exercise1], exercise2, exercises[exercise2])
 
 if __name__ == '__main__':
     app.run(debug=True)
