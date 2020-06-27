ca425459a6ca2c61cbd037963509118896e56bc1
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-06-27 16:56:37 -0400

    got some things workin.

--- loading.py
+++ loading.py
@@ -2,19 +2,19 @@ loading = {
     'scap pull ups':
        {
             'exercises': ['pull up','bar muscle up','ring muscle up'],
-            'reps': ['20 reps'],
+            'reps': '20 reps',
             'img': 'https://tinyurl.com/ybljszp6'
        },
     'kipping warm up':
        {
             'exercises': ['pull up','bar muscle up','ring muscle up'],
-            'reps': ['25 reps'],
+            'reps': '25 reps',
             'img': 'https://tinyurl.com/yap8m62n'
        },
     'barrel rolls':
         {
             'exercises': ['none', 'potato'],
-            'reps': ['vodka martini', 'blastocyst'],
-            'img': ['sure.com']
+            'reps': '5',
+            'img': 'sure.com',
         }
 }
