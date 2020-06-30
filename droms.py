79f56770e98b03e19739754dc4117d29c67a78c7
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-06-30 07:49:57 -0400

    alphabatized droms.py

--- droms.py
+++ droms.py
@@ -1,173 +1,175 @@
 droms = {
-    'bear crawls': {
-        'categories': ['snatches', 'cleans', 'core', 'deadlifts', 'saggital presses', 'gymanstics upper',
-                       'gymnastics lower', 'kettlebells'],
-        'time': 2,
-        'reps': ['2 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://tinyurl.com/yasf6uep',
-
-    },
-    'cossack lunges': {
-        'categories': ['squats', 'cleans', 'deadlifts', 'lunges', 'gymnastics lower', 'kettlebells'],
-        'time': 2,
-        'reps': ['10 reps', '20 reps', '30 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://tinyurl.com/yajsco2y'
-    },
-    'walking lunges': {
-        'categories': ['squats', 'cleans', 'deadlifts', 'lunges', 'gymnastics lower', 'kettlebells'],
-        'time': 1,
-        'reps': ['10 reps', '20 reps', '30 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://tinyurl.com/y8a86uhh'
-    },
     'air squats': {
-        'categories': ['squats', 'squats', 'squats', 'cleans', 'deadlifts', 'gymnastics lower'],
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'squats', 'squats', 'squats'],
         'time': 1,
         'reps': ['10 reps', '20 reps', '30 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://thumbs.gfycat.com/ParchedSpitefulDeviltasmanian-size_restricted.gif'
-
-    },
-    'spidermans': {
-        'categories': ['squats', 'cleans', 'deadlifts', 'snatches', 'gymnastics lower'],
-        'time': 3,
-        'reps': ['2 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media1.tenor.com/images/1d67a12af3ce871fd0601723bf86f5c2/tenor.gif?itemid=10818779'
-
-    },
-    'worlds greatest stretch': {
-        'categories': ['squats', 'cleans', 'deadlifts', 'snatches', 'lunges', 'gymnastics lower', 'gymnastics upper'],
-        'time': 3,
-        'reps': ['3 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://misterbackpain.com/wp-content/uploads/2020/01/worlds-greatest-stretch.gif'
-
-    },
-    'inchworms': {
-        'categories': ['saggital presses', 'squats', 'cleans', 'deadlifts', 'gymnastics lower', 'gymnastics upper',
-                       'kettlebells', 'overhead presses'],
-        'time': 2,
-        'reps': ['2 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media1.giphy.com/media/UTXzXAwUHGx8MDEtPS/giphy-downsized.gif'
-
-    },
-    'thoracic bridge': {
-        'categories': ['saggital presses', 'cleans', 'snatches', 'gymnastics upper', 'kettlebells', 'overhead presses'],
-        'time': 2,
-        'reps': ['2 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://jensinkler.com/wp-content/uploads/2019/06/Bodyweight-Thoracic-Bridge-With-Reach.gif'
-
-    },
-    'walking forward kicks': {
-        'categories': ['squats', 'cleans', 'snatches', 'jerks', 'lunges', 'deadlifts', 'kettlebells'],
-        'time': 1,
-        'reps': ['10 reps', '20 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://i0.wp.com/thumbs.gfycat.com/MarvelousRespectfulLeech-size_restricted.gif?w=1155&h=840'
-
-    },
-    'walking back kicks': {
-        'categories': ['squats', 'cleans', 'snatches', 'jerks', 'lunges', 'deadlifts', 'kettlebells'],
-        'time': 1,
-        'reps': ['10 reps', '20 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media.tenor.com/images/c50ca435dffdb837914e7cb32c1e7edf/tenor.gif'
-
-    },
-    'frog walk': {
-        'categories': ['squats', 'snatches', 'cleans'],
-        'time': 2,
-        'reps': ['1 min'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://i.pinimg.com/originals/ca/f1/29/caf12983c2cfa59546814d3051f423df.gif'
-
     },
     'arm circles': {
-        'categories': ['saggital presses', 'jerks', 'snatches', 'gymnastics upper', 'overhead presses'],
+        'categories': ['gymnastics upper', 'jerks', 'overhead presses', 'saggital presses', 'snatches'],
         'time': 1,
         'reps': ['20 forward, 20 backward'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://media2.giphy.com/media/11sJmQcnMcMeQM/200.gif'
-
     },
     'banded side steps': {
-        'categories': ['squats', 'cleans', 'jerks', 'snatches', 'deadlifts','lunges'],
+        'categories': ['cleans', 'deadlifts', 'jerks', 'lunges', 'snatches', 'squats'],
         'time': 2,
         'reps': ['1 min', '2 min'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media4.s-nbcnews.com/j/newscms/2019_05/2738466'
-               '/better_standing_lateral_band_walk_4a7c397f4668e1be769cc7b6bddc339f.fit-560w.gif '
-
+        'img': 'https://media4.s-nbcnews.com/j/newscms/2019_05/2738466/better_standing_lateral_band_walk_4a7c397f4668e1be769cc7b6bddc339f.fit-560w.gif '
     },
-    'seal walk': {
-        'categories': ['saggital presses', 'jerks', 'gymnastics upper'],
+    'bear crawls': {
+        'categories': ['cleans', 'core', 'deadlifts', 'gymanstics upper', 'gymnastics lower', 'kettlebells',
+                       'saggital presses', 'snatches', 'squats'],
         'time': 2,
-        'reps': ['10 seal walks forward, turn around, 10 back', '20 seal walks forward, turn around, 20 back'],
+        'reps': ['2 min'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media.giphy.com/media/8Ag4AORS8xPYHdeU6f/giphy.gif'
-
+        'img': 'https://tinyurl.com/yasf6uep'
     },
-    'push ups': {
-        'categories': ['jerks', 'snatches', 'saggital presses', 'gymnastics upper'],
+    'broad jumps': {
+        'categories': ['cleans', 'jerks', 'kettlebells', 'plyos', 'snatches'],
         'time': 1,
-        'reps': ['15 reps,', '20 reps', '25 reps'],
+        'reps': ['10 reps,', '20 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
-
     },
     'burpees': {
-        'categories': ['saggital presses', 'gymnastics upper', 'gymnastics lower'],
-        'time': 1,
-        'reps': ['15 reps,', '20 reps', '25 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
-
-    },
-    'shoulder passthroughs': {
-        'categories': ['jerks', 'snatches', 'overhead presses', 'saggital presses', 'gymnastics upper'],
-        'time': 1,
-        'reps': ['15 reps,', '20 reps', '25 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
-    },
-    'supermans': {
-        'categories': ['gymnastics upper', 'core', 'deadlifts'],
-        'time': 1,
-        'reps': ['15 reps,', '20 reps', '25 reps'],
-        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
-    },
-    'dead bugs': {
-        'categories': ['gymnastics upper', 'core', 'deadlifts', 'squats', 'cleans', 'jerks'],
+        'categories': ['gymnastics lower', 'gymnastics upper', 'saggital presses'],
         'time': 1,
         'reps': ['15 reps,', '20 reps', '25 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
     },
     'core rolling': {
-        'categories': ['gymnastics upper', 'core', 'deadlifts', 'squats', 'cleans', 'jerks'],
+        'categories': ['cleans', 'core', 'deadlifts', 'gymnastics upper', 'jerks', 'squats'],
         'time': 2,
         'reps': ['10 reps,', '20 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
     },
+    'cossack lunges': {
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'kettlebells', 'lunges', 'squats'],
+        'time': 2,
+        'reps': ['10 reps', '20 reps', '30 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://tinyurl.com/yajsco2y'
+    },
+    'dead bugs': {
+        'categories': ['cleans', 'core', 'deadlifts', 'gymnastics upper', 'jerks', 'squats'],
+        'time': 1,
+        'reps': ['15 reps,', '20 reps', '25 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
+    },
+    'frog walk': {
+        'categories': ['cleans', 'snatches', 'squats'],
+        'time': 2,
+        'reps': ['1 min'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://i.pinimg.com/originals/ca/f1/29/caf12983c2cfa59546814d3051f423df.gif'
+    },
+    'inchworms': {
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'gymnastics upper', 'kettlebells',
+                       'overhead presses', 'saggital presses', 'squats'],
+        'time': 2,
+        'reps': ['2 min'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media1.giphy.com/media/UTXzXAwUHGx8MDEtPS/giphy-downsized.gif'
+    },
+    'push ups': {
+        'categories': ['gymnastics upper', 'jerks', 'saggital presses', 'snatches'],
+        'time': 1,
+        'reps': ['15 reps,', '20 reps', '25 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
+    },
+    'seal walk': {
+        'categories': ['gymnastics upper', 'jerks', 'saggital presses'],
+        'time': 2,
+        'reps': ['10 seal walks forward, turn around, 10 back',
+                 '20 seal walks forward, turn around, 20 back'
+                 ],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media.giphy.com/media/8Ag4AORS8xPYHdeU6f/giphy.gif'
+    },
+    'shoulder passthroughs': {
+        'categories': ['gymnastics upper', 'jerks', 'overhead presses', 'saggital presses', 'snatches'],
+        'time': 1,
+        'reps': ['15 reps,', '20 reps', '25 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
+    },
+    'spidermans': {
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'snatches', 'squats'],
+        'time': 3,
+        'reps': ['2 min'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media1.tenor.com/images/1d67a12af3ce871fd0601723bf86f5c2/tenor.gif?itemid=10818779'
+    },
+    'supermans': {
+        'categories': ['core', 'deadlifts'],
+        'time': 1,
+        'reps': ['15 reps,', '20 reps', '25 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
+    },
+    'thoracic bridge': {
+        'categories': ['cleans', 'gymnastics upper', 'kettlebells', 'overhead presses', 'saggital presses', 'snatches'],
+        'time': 2,
+        'reps': ['2 min'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://jensinkler.com/wp-content/uploads/2019/06/Bodyweight-Thoracic-Bridge-With-Reach.gif'
+    },
     'vertical jumps': {
-        'categories': ['cleans', 'jerks', 'snatches', 'plyos'],
+        'categories': ['cleans', 'jerks', 'plyos', 'snatches'],
         'time': 1,
         'reps': ['10 reps,', '20 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
         'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
     },
-    'broad jumps': {
-        'categories': ['cleans', 'jerks', 'snatches', 'plyos', 'kettlebells'],
+    'walking back kicks': {
+        'categories': ['cleans', 'deadlifts', 'jerks', 'kettlebells', 'lunges', 'snatches', 'squats'],
         'time': 1,
-        'reps': ['10 reps,', '20 reps'],
+        'reps': ['10 reps', '20 reps'],
         'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
-        'img': 'https://media3.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif'
+        'img': 'https://media.tenor.com/images/c50ca435dffdb837914e7cb32c1e7edf/tenor.gif'
+    },
+    'walking forward kicks': {
+        'categories': ['cleans', 'deadlifts', 'jerks', 'kettlebells', 'lunges', 'snatches', 'squats'],
+        'time': 1,
+        'reps': ['10 reps', '20 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://i0.wp.com/thumbs.gfycat.com/MarvelousRespectfulLeech-size_restricted.gif?w=1155&h=840'
+    },
+    'walking lunges': {
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'kettlebells', 'lunges', 'squats'],
+        'time': 1,
+        'reps': ['10 reps', '20 reps', '30 reps'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://tinyurl.com/y8a86uhh'
+    },
+    'worlds greatest stretch': {
+        'categories': ['cleans', 'deadlifts', 'gymnastics lower', 'gymnastics upper', 'lunges', 'snatches', 'squats'],
+        'time': 3,
+        'reps': ['3 min'],
+        'url': 'https://www.youtube.com/watch?v=a8vaVbT_lX0',
+        'img': 'https://misterbackpain.com/wp-content/uploads/2020/01/worlds-greatest-stretch.gif'
     },
 }
+# I USED THIS CODE TO ALPHABATIZE MY OLD DICT THAT IS NOW {DROMS} ##
+
+from collections import OrderedDict
+
+# for i in sorted (droms) :
+#     print ((i, droms[i]), end =" ")
+#
+# alpha = OrderedDict(sorted(droms.items(), key=lambda x: x[0]))
+#
+# for k, v in alpha.items():
+#     print(k, v)
+
+# for i, j in droms2.items():
+#     sorted_dict = {i: sorted(j['categories'])}
+#     print(sorted_dict)
