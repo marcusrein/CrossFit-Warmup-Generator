a28c8a5d6cbc50b8e8382c6e8c8bc50bb824c261
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-07-04 13:21:22 -0400

    minor edits

--- barbell_warmups.py
+++ barbell_warmups.py
@@ -1,5 +1,5 @@
 barbell_warmups = {
-    'Basic Burgener Warmup' :
+    'Basic Burgener Warmup With PVC Pipe' :
         {
             'category': ['cleans', 'snatches', 'jerks'],
             'url': 'https://youtu.be/mWffzvDGeg4',
@@ -7,7 +7,7 @@ barbell_warmups = {
             'text': '1. Down and up \n 2. Elbows high and outside \n 3.Muscle snatch \n 4. Snatch land \n 5. Snatch drop '
                     '\n 6. Hang power snatch'
         },
-    'Progressive Loading':
+    'Progressive Barbell Loading':
         {
             'category': ['squats','deadlifts','cleans','snatches','jerks']
             'text': 'Warm-Up Set #1: 40% of your 1RM x 5 \n Warm-Up Set #2: 50% of your 1RM x 5 \n Warm-Up Set #3: '
