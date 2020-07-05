c3b6d3a23cad37dd0e35a45d62bbb4acc3c97492
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-07-05 12:40:58 -0400

    made TODO notes

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from barbell_warmups_dict import *
 
 app = Flask(__name__)
 
-
-# TODO: see line 270 on getters.py
+# TODO: Fix selected movements output (see bottom of index.html and how it doesn't output anything. por que?
+# TODO: Import stickies copy/paste when barbell wod is ready again
 # TODO: 1. Create DB category throughout code, 2. fix bug in index.html that doubles-up input (it lookslike
 #  I can put in airsquat 2x) 3. create 'equipment' thing. 4. have users log in 5. figure out barbell loading
 
