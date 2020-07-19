56734b98ea24adf738e3bea30fbb745fbc86f748
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-07-19 09:21:41 -0400

    trying front end login/logout stuff.

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from getters import *
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from barbell_warmups import *
 from kb_warmups import *
 from media import *
 
 app = Flask(__name__)
+
+app.secret_key = 'crossfit'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You were just logged in')
+            return redirect(url_for('first_page'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out')
+
+    return redirect(url_for('/'))
 
 #TODO testing this code to go into master
 
