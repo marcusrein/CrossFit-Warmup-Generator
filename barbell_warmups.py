990d94f5fdb38bea37e842b55146dfb1b5729bdb
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-08-02 06:47:40 -0400

    updating bb warmups

--- barbell_warmups.py
+++ barbell_warmups.py
@@ -6,9 +6,8 @@ barbell_warmups_dict = {
         {
             'category': ['cleans', 'snatches', 'jerks'],
             'url': 'https://youtu.be/mWffzvDGeg4',
-            'img': 'https://i.imgur.com/bT38k9u.gif',
-            'text': '1. Down and up \n 2. Elbows high and outside \n 3.Muscle snatch \n 4. Snatch land \n 5. Snatch '
-                    'drop \n 6. Hang power snatch',
+            'img': 'https://j.gifs.com/p85RJm.gif',
+            'text': 'Use a PVC Pipe or Empty Barbell and Follow The Video\'s Instructions',
             'reps': ['1 Round', '1 Round', '1 Round'],
         },
     'Barbell Clean Warmup':
@@ -16,32 +15,32 @@ barbell_warmups_dict = {
             'category': ['cleans'],
             'url': 'https://www.youtube.com/watch?v=1_IK2yYA3ig',
             'img': 'https://thumbs.gfycat.com/SarcasticEnergeticHalcyon-max-1mb.gif',
-            'text': 'Follow the instructional video',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Use an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Overhead Warmup':
         {
             'category': ['snatches', 'jerks', 'overhead squats'],
             'url': 'https://www.youtube.com/watch?v=1_IK2yYA3ig',
             'img': 'https://media2.giphy.com/media/ZBn6RZxzQp5Rrov88u/giphy.gif',
-            'text': '5-10 BB Front Squats, 5-10 BB Strict Press, 5-10 BB Back Squats, 5-10 BB Overhead Squats',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Jerk Warmup':
         {
             'category': ['jerks'],
             'url': 'https://www.youtube.com/watch?v=oaUrbXs5fe0',
             'img': 'https://thumbs.gfycat.com/FlawedDazzlingBengaltiger-small.gif',
-            'text': 'Follow the instructional video',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Snatch Warmup':
         {
             'category': ['snatches'],
             'url': 'https://www.youtube.com/watch?v=lN4wmP0n9do',
             'img': 'https://media.tenor.com/images/63673ac0244c6f70e8f8cc3df7231076/tenor.gif',
-            'text': 'Follow the instructional video. Take 8min to build to your working weight.',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
     'Barbell Loading for Pressing':
@@ -49,9 +48,8 @@ barbell_warmups_dict = {
             'category': ['presses'],
             'url': 'https://www.youtube.com/watch?v=uf2s3KFQWfI',
             'img': 'https://thumbs.gfycat.com/CourteousWellwornHammerkop-max-1mb.gif',
-            'text': 'Warm-Up Set #1: 40% of your 1RM x 5 \n Warm-Up Set #2: 50% of your 1RM x 5 \n Warm-Up Set #3: '
-                    '60% of your 1RM x 3',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
     'Barbell Loading for Squatting':
@@ -59,9 +57,8 @@ barbell_warmups_dict = {
             'category': ['squats'],
             'url': 'https://youtu.be/pCEpcIo-O4I',
             'img': 'https://i.makeagif.com/media/11-12-2015/F83Osw.gif',
-            'text': 'Warm-Up Set #1: 40% of your 1RM x 5 \n Warm-Up Set #2: 50% of your 1RM x 5 \n Warm-Up Set #3: '
-                    '60% of your 1RM x 3',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
 
         },
@@ -70,9 +67,8 @@ barbell_warmups_dict = {
             'category': ['deadlifts'],
             'url': 'https://www.youtube.com/watch?v=Qpr5i4DTpk0',
             'img': 'https://media4.giphy.com/media/ydPTjG5XbHVEQ/giphy-downsized.gif',
-            'text': 'Warm-Up Set #1: 40% of your 1RM x 5 \n Warm-Up Set #2: 50% of your 1RM x 5 \n Warm-Up Set #3: '
-                    '60% of your 1RM x 3',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
     'Barbell Loading for Lunging':
@@ -80,8 +76,8 @@ barbell_warmups_dict = {
             'category': ['lunges'],
             'url': 'https://www.youtube.com/watch?v=ci4rsmlOk24',
             'img': 'https://dailyburn.com/life/wp-content/uploads/2017/10/Barbell-Lunge.gif',
-            'text': 'Focus on technique',
-            'reps': ['3 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
+            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
 }
