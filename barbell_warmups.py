a17de48b03d55732195b2600029d04570343f854
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-08-02 12:20:41 -0400

    new things for bb warmups

--- barbell_warmups.py
+++ barbell_warmups.py
@@ -13,9 +13,9 @@ barbell_warmups_dict = {
     'Barbell Clean Warmup':
         {
             'category': ['cleans'],
-            'url': 'https://www.youtube.com/watch?v=1_IK2yYA3ig',
+            'url': 'https://www.youtube.com/watch?v=_3WeR9NdflI',
             'img': 'https://thumbs.gfycat.com/SarcasticEnergeticHalcyon-max-1mb.gif',
-            'text': 'Use an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Overhead Warmup':
@@ -23,7 +23,7 @@ barbell_warmups_dict = {
             'category': ['snatches', 'jerks', 'overhead squats'],
             'url': 'https://www.youtube.com/watch?v=1_IK2yYA3ig',
             'img': 'https://media2.giphy.com/media/ZBn6RZxzQp5Rrov88u/giphy.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Jerk Warmup':
@@ -31,7 +31,7 @@ barbell_warmups_dict = {
             'category': ['jerks'],
             'url': 'https://www.youtube.com/watch?v=oaUrbXs5fe0',
             'img': 'https://thumbs.gfycat.com/FlawedDazzlingBengaltiger-small.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
         },
     'Barbell Snatch Warmup':
@@ -39,7 +39,7 @@ barbell_warmups_dict = {
             'category': ['snatches'],
             'url': 'https://www.youtube.com/watch?v=lN4wmP0n9do',
             'img': 'https://media.tenor.com/images/63673ac0244c6f70e8f8cc3df7231076/tenor.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
@@ -48,7 +48,7 @@ barbell_warmups_dict = {
             'category': ['presses'],
             'url': 'https://www.youtube.com/watch?v=uf2s3KFQWfI',
             'img': 'https://thumbs.gfycat.com/CourteousWellwornHammerkop-max-1mb.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
@@ -57,7 +57,7 @@ barbell_warmups_dict = {
             'category': ['squats'],
             'url': 'https://youtu.be/pCEpcIo-O4I',
             'img': 'https://i.makeagif.com/media/11-12-2015/F83Osw.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
 
@@ -67,7 +67,7 @@ barbell_warmups_dict = {
             'category': ['deadlifts'],
             'url': 'https://www.youtube.com/watch?v=Qpr5i4DTpk0',
             'img': 'https://media4.giphy.com/media/ydPTjG5XbHVEQ/giphy-downsized.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
@@ -76,7 +76,7 @@ barbell_warmups_dict = {
             'category': ['lunges'],
             'url': 'https://www.youtube.com/watch?v=ci4rsmlOk24',
             'img': 'https://dailyburn.com/life/wp-content/uploads/2017/10/Barbell-Lunge.gif',
-            'text': 'Start with an Empty Barbell and Follow The Video\'s Instructions',
+            'text': 'Add weight each set. Get to your working weight.',
             'reps': ['2-4 Warmup Sets', '3-4 Warmup Sets', '3-5 Warmup Sets'],
 
         },
