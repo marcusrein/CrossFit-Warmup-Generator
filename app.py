5d19fe54aea73e9c531b46f4e8fdfb7de0305d89
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-08-04 17:12:00 -0400

    finished lesson 4

--- app.py
+++ app.py
@@ -25,15 +25,14 @@ class User(db.Model):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.Model):
+class Gear(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
+    gear_name = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Gear('{self.gear_name}')"
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
