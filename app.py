96fe2f0a74b7a7e9f1aef610112fed1501b537e4
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-08-04 11:27:51 -0400

    finished lesson 4. great stuff

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from flask_bcrypt import Bcrypt
 from barbell_warmups import *
 from kb_warmups import *
 from getters import *
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '2fc1cdd26003685f749bd3e217824aca'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    gear = db.relationship('Gear', backref='athlete', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -17,6 +42,7 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('first_page'))
     return render_template('register.html', title='Register', form=form)
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -28,6 +54,7 @@ def login():
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 @app.route('/logout')
 def logout():
