a57be1a31ad88291cb5b24dd61b00dbb923ed2e4
marcusrein <64141593+marcusrein@users.noreply.github.com>
2020-08-12 10:01:31 -0400

    gunicorn ' gunicorn --bind 127.0.0.1:8000 run:app '

--- appfolder/__init__.py
+++ appfolder/__init__.py
@@ -6,6 +6,7 @@ from flask_mail import Mail
 import os
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
