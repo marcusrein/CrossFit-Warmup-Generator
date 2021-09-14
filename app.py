bab133e1b5862bad39e27ced05c2bcb32e9d7532
marcus <marcus.rein@gmail.com>
2021-09-14 05:23:41 -0400

    improved spacing on header

--- app.py
+++ app.py
@@ -2,18 +2,11 @@ from appfolder import app
 
 # JJ HELP TODOS
 # TODO: have accordions open individually as well as when you click their "folder" they also all open or close
-#  TODO: * Get screen orientation to lock in portrait mode
 #
-
 # MDR TODOS
-# TODO: Feedback input for users
 # TODO: Review all categories and get them right. Ensure spelling, plurals, alphabeticalness, and make sure I have categories I want exactly where I want them. Think it through, write it out!
 # TODO: Continue to update gifs/vids (ended on kb_warmups.py)
-# TODO: need to differentiate olympic lift warmup and strength/squat warmup. If olympic lift is in 'easy' warm up one way, if olympic lift is 'tough' warm up another. If strength movement is combined with oly movement in a WOD...?
-# TODO: need an overhead squat category. see barbellwarmups.py 'overhead squat warmup'
 
-# LONG TERM TODOS:
-# TODO: add draggable inputs
 
 
 if __name__ == '__main__':
